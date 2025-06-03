4c9750a3d97804dd61b9406967809351c1485f26
beefwad13 <beefwellington@gmail.com>
2025-06-02 21:41:16 -0400

    latested game

--- scenes/MainMenu.js
+++ scenes/MainMenu.js
@@ -7,9 +7,7 @@ class MainMenu extends Phaser.Scene {
 
         // Get the center coordinates of the game
         const centerX = this.cameras.main.centerX;
-        const centerY = this.cameras.main.centerY;
-
-        // Create the title
+        const centerY = this.cameras.main.centerY;        // Create the title
         const title = this.add.text(centerX, centerY - 100, 'Not A Doom Shooter', {
             font: '48px Arial',
             fill: '#ffffff',
@@ -17,8 +15,24 @@ class MainMenu extends Phaser.Scene {
             padding: { x: 20, y: 10 }
         }).setOrigin(0.5);
 
+        // Add game instructions
+        const instructions = [
+            'GAME NOTES:',
+            'Enemies spawn in waves every 15 seconds.',
+            'Armor absorbs 1/3 of damage.',
+            'Select 1 of 3 random upgrades when you level up.'
+        ];
+        
+        instructions.forEach((text, index) => {
+            this.add.text(centerX, centerY - 20 + (index * 25), text, {
+                font: '16px Arial',
+                fill: '#cccccc',
+                align: 'center'
+            }).setOrigin(0.5);
+        });
+
         // Create the start button
-        const startButton = this.add.text(centerX, centerY, 'Start Game', {
+        const startButton = this.add.text(centerX, centerY + 100, 'Start Game', {
             font: '32px Arial',
             fill: '#ffffff',
             backgroundColor: '#333333',
